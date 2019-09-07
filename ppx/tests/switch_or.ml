1282ea34ffb1f4efd9cde84747d9a6ed90bb4184
Jun Furuse <jun.furuse@dailambda.jp>
2019-09-07 05:07:18 +0000

    Implemented assert false and removed opcodes after FAIL

--- ppx/tests/switch_or.ml
+++ ppx/tests/switch_or.ml
@@ -1,9 +1,15 @@
 open SCaml
 let main (x:unit) y = 
   ([], 
-   match (Left (Int 1) : (int, int) sum) with
-   | Left x -> assert (x = Int 1)
-   | Right x -> assert false)
+   assert 
+     ((match (Left (Int 1) : (int, int) sum) with
+         | Left x -> x = Int 1
+         | Right x -> assert false)
+      &&
+      (match (Right (Int 2) : (int, int) sum) with
+       | Left x -> assert false
+       | Right x -> x = Int 2)))
+  
 
      
     
