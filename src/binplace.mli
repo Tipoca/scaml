b33909f6ee687a2521cec7ad6a8ae86c2434d2f9
Jun Furuse <jun.furuse@dailambda.jp>
2019-12-08 09:54:56 +0000

    doc

--- src/binplace.mli
+++ src/binplace.mli
@@ -1,3 +1,7 @@
+(* This module defines encoding of ML records and variants in Michelson,
+   which only has binary tree based type: [pair] and [or].
+*)
+
 type side = Left | Right
 
 type 'a tree =
@@ -5,7 +9,12 @@ type 'a tree =
   | Branch of 'a tree * 'a tree
 
 val place : 'a list -> 'a tree
-
+(** Layout the list of elements in a binary tree *)
+    
 val path : int -> int -> side list
+(** [path i n] finds out how to access the [i]-th element of [n]
+    in the layout by [place].
+*)
 
 val fold : leaf:('a -> 'b) -> branch:('b -> 'b -> 'b) -> 'a tree -> 'b
+(** Folding over [tree] *)
