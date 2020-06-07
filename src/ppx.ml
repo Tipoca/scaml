86acdb704ea213dd8e38e4e1f8130647e04d516a
Jun Furuse <jun.furuse@dailambda.jp>
2020-06-07 15:08:39 +0000

    Linking message

--- src/ppx.ml
+++ src/ppx.ml
@@ -2,12 +2,12 @@ let rev_modules = ref []
 
 let register s = 
   let module_ : SCamlComp.Module.t = Marshal.from_string s 0 in
-  Format.eprintf "Got %a@." SCamlComp.Module.pp module_;
+  (* Format.eprintf "@[<2>Got %a@]@." SCamlComp.Module.pp module_; *)
   rev_modules := module_ :: !rev_modules
 
 let emit ~outputprefix = 
   let modules = List.rev !rev_modules in
-  Format.eprintf "Linking %s...@." (String.concat "," (List.map (fun m -> m.SCamlComp.Module.name) modules));
+  Format.eprintf "Linking %s.tz using %s...@." outputprefix (String.concat "," (List.map (fun m -> m.SCamlComp.Module.name) modules));
   try SCamlComp.link (Some outputprefix) modules with
   | Location.Error e ->
       Location.print_report Format.err_formatter e;
