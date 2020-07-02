6d8741fea38123975c5c347e230bd803106ec1a4
Jun Furuse <jun.furuse@dailambda.jp>
2020-07-02 05:56:46 +0000

    comment

--- src/varname.mli
+++ src/varname.mli
@@ -12,9 +12,4 @@
 (*                                                                        *)
 (**************************************************************************)
 
-val variant_type
-  : Env.t
-  -> Types.type_expr
-  -> Types.constructor_description list
-  -> string list option (* nullaries *)
-     * (string * Types.type_expr list) list (* non nullaries *)
+val create : string -> Michelson.Type.t -> Ident.t
