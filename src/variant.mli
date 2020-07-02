baf2a51a4386aa497ea0b60168a0895aab1d129b
Jun Furuse <jun.furuse@dailambda.jp>
2020-07-02 05:56:46 +0000

    comment

--- src/variant.mli
+++ src/variant.mli
@@ -12,8 +12,9 @@
 (*                                                                        *)
 (**************************************************************************)
 
-val variant_type : 
-  Env.t ->
-  Types.type_expr ->
-  Types.constructor_description list ->
-  string list option * (string * Types.type_expr list) list
+val variant_type
+  : Env.t
+  -> Types.type_expr
+  -> Types.constructor_description list
+  -> string list option (* nullaries *)
+     * (string * Types.type_expr list) list (* non nullaries *)
